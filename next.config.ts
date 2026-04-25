955770287bdfd959c6d588d3065058f379d381b8
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-04-26 06:48:18 +0900

    fix: update image remote patterns for production

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,24 @@
-import type { NextConfig } from 'next';
-const { i18n } = require('./next-i18next.config');
+import type { NextConfig } from "next";
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	i18n,
-	env: {
-		REACT_APP_API_URL: process.env.REACT_APP_API_URL,
-		REACT_APP_API_GRAPHQL_URL: process.env.REACT_APP_API_GRAPHQL_URL,
-		REACT_APP_API_WS: process.env.REACT_APP_API_WS,
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '3004',
-				pathname: '/uploads/**',
-			},
-		],
-	},
+  reactStrictMode: true,
+  i18n,
+  env: {
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+    REACT_APP_API_GRAPHQL_URL: process.env.REACT_APP_API_GRAPHQL_URL,
+    REACT_APP_API_WS: process.env.REACT_APP_API_WS,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4001",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
